58841a4a1980c53e9fb5007d212053e3478de816
RayanceKing <88320297+RayanceKing@users.noreply.github.com>
2026-03-10 18:32:16 +0800

    Initial Commit

---
+++ VisualNovelReader/VisualNovelReaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  VisualNovelReaderApp.swift
+//  VisualNovelReader
+//
+//  Created by rayanceking on 2026/3/10.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VisualNovelReaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ VisualNovelReaderTests/VisualNovelReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisualNovelReaderTests.swift
+//  VisualNovelReaderTests
+//
+//  Created by rayanceking on 2026/3/10.
+//
+
+import Testing
+@testable import VisualNovelReader
+
+struct VisualNovelReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
